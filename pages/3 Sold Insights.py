e6d5f733acfcec89b28f36adb46159f5f8adbc4f
Abdoo Mostafa <149151985+abdoomostafa99@users.noreply.github.com>
2024-03-26 15:44:40 +0200

    Update 3 Sold Insights.py

--- pages/3 Sold Insights.py
+++ pages/3 Sold Insights.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import seaborn as sns
 import random
 import requests
 from streamlit_lottie import st_lottie
